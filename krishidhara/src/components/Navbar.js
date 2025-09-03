cee51c6cf6ba2ed2aae48764a04b8350d6b65050
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-09-04 01:25:00 +0530

    homepage

--- krishidhara/src/components/Navbar.js
+++ krishidhara/src/components/Navbar.js
@@ -1,32 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Navbar.css";
-import krishiLogo from "../assets/krishiLogo.png"; 
+import krishiLogo from "../assets/krishiLogo.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="navbar">
       <div className="navbar-container">
         
-        {/* Logo */}
+        {/* Logo + Name */}
         <div className="navbar-logo">
-          <img src={krishiLogo} alt="Krishidhara Logo" />
-          <span>Krishidhara</span>
+          <img src={krishiLogo} alt="Krishidhara Logo" className="logo-img" />
+          <span className="brand-name">Krishidhara</span>
         </div>
 
-        {/* Nav Links */}
-        <nav className="navbar-links">
-          <a href="#crop-finder">Crop Finder</a>
-          <a href="#marketplace">Marketplace</a>
-          <a href="#stories">Stories</a>
-          <a href="#mentorship">Mentorship</a>
-        </nav>
-
-        {/* Buttons */}
-        <div className="navbar-buttons">
-          <button className="nav-cta">Learn & Earn</button>
-          <button className="nav-primary">Find Your Crop</button>
+        {/* Hamburger Icon (Mobile) */}
+        <div
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span><span></span><span></span>
         </div>
 
+        {/* Menu (Links + Buttons) */}
+        <div className={`menu ${menuOpen ? "open" : ""}`}>
+          <nav className="navbar-links">
+            <a href="#crop-finder" onClick={() => setMenuOpen(false)}>Crop Finder</a>
+            <a href="#marketplace" onClick={() => setMenuOpen(false)}>Marketplace</a>
+            <a href="#stories" onClick={() => setMenuOpen(false)}>Stories</a>
+            <a href="#mentorship" onClick={() => setMenuOpen(false)}>Mentorship</a>
+          </nav>
+
+          <div className="navbar-buttons">
+            <button className="nav-cta" onClick={() => setMenuOpen(false)}>Learn & Earn</button>
+            <button className="nav-primary" onClick={() => setMenuOpen(false)}>Find Your Crop</button>
+          </div>
+        </div>
       </div>
     </header>
   );
