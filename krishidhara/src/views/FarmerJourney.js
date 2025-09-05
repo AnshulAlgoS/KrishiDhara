1e87d652155296d3c23512c7e2d01060a5f8ebb5
Mahira Khan <153006911+Walker71619@users.noreply.github.com>
2025-09-05 14:33:43 +0530

    Add files via upload

---
+++ krishidhara/src/views/FarmerJourney.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import VideoSection from "../components/VideoSection";
+
+
+const FarmerJourney = () => {
+  return (
+    <>
+      <Navbar />
+      <VideoSection />
+    </>
+  );
+};
+
+export default FarmerJourney;
